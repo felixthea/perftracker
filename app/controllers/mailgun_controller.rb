335455695fdda033bf2a1f8ab7480062cee42f1a
Felix Thea <felix.thea@placeexchange.com>
2025-02-23 22:19:59 -0500

    logging

--- app/controllers/mailgun_controller.rb
+++ app/controllers/mailgun_controller.rb
@@ -3,8 +3,8 @@ class MailgunController < ApplicationController
 
   def incoming
     # Parse the Mailgun JSON payload
-    Rails.logger.info ("Email params from line 6=#{params}")
-    email_params = params.permit(:recipient, :sender, :subject, :body_plain)
+    email_params = params.permit(:recipient, :sender, :subject, "body_plain")
+    Rails.logger.info ("Email params from line 7=#{email_params}")
 
     # Log for debugging (optional)
     Rails.logger.info "Received email from #{email_params[:sender]} to #{email_params[:recipient]}"
