7127b564d4c8c66c0c8a95f06043f3ce68e7c58c
Felix Thea <felix.thea@placeexchange.com>
2025-02-23 22:26:31 -0500

    fix

--- app/controllers/mailgun_controller.rb
+++ app/controllers/mailgun_controller.rb
@@ -4,13 +4,11 @@ class MailgunController < ApplicationController
   def incoming
     # Parse the Mailgun JSON payload
     # email_params = params.permit(:recipient, :sender, :subject, "body_plain")
-    # Rails.logger.info ("Email params from line 7=#{email_params}")
-    Rails.logger.info("line 8=#{params["body-plain"]}")
 
     # Log for debugging (optional)
-    # Rails.logger.info "Received email from #{email_params[:sender]} to #{email_params[:recipient]}"
+    Rails.logger.info "Received email from #{email_params[:sender]} to #{email_params[:recipient]}"
 
-    # ProcessAccomplishmentsEmail.new(params.permit(:sender, :recipient, :subject, :body_plain)).call
+    ProcessAccomplishmentsEmail.new(params).call
 
     # Respond to Mailgun to acknowledge receipt
     render json: { status: "success" }, status: 200
