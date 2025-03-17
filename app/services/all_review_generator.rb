556a7426ed360019f7c1e22f4c31eac2891497d0
Felix Thea <felix.thea@placeexchange.com>
2025-03-16 21:56:33 -0400

    remove time requirement

--- app/services/all_review_generator.rb
+++ app/services/all_review_generator.rb
@@ -24,24 +24,24 @@ class AllReviewGenerator
       time_in_user_tz = Time.current.in_time_zone(user.timezone)
 
       # Only send this report if it's Friday at 6pm the user's timezone
-      if time_in_user_tz.hour == SEND_HOUR && time_in_user_tz.wday == 5
-        # In the future we may ask the user to specify which expectation to use
-        most_recent_expectation = Expectation.where(user_id: user.id).order(created_at: :desc).first
-        review_generator = ReviewGenerator.new(
-          user_id: user.id,
-          start_time: @start_time,
-          end_time: @end_time,
-          expectation_id: most_recent_expectation.id,
-          review_type: @review_type,
-          initial_prompt: @initial_prompt
-        )
+      # if time_in_user_tz.hour == SEND_HOUR && time_in_user_tz.wday == 5
+      # In the future we may ask the user to specify which expectation to use
+      most_recent_expectation = Expectation.where(user_id: user.id).order(created_at: :desc).first
+      review_generator = ReviewGenerator.new(
+        user_id: user.id,
+        start_time: @start_time,
+        end_time: @end_time,
+        expectation_id: most_recent_expectation.id,
+        review_type: @review_type,
+        initial_prompt: @initial_prompt
+      )
 
-        review = review_generator.call
+      review = review_generator.call
 
-        if !review.nil?
-          MailgunService.send_email("felixthea@gmail.com", "Hello!", review.result)
-        end
+      if !review.nil?
+        MailgunService.send_email("felixthea@gmail.com", "Hello!", review.result)
       end
+      # end
     end
   end
 end
