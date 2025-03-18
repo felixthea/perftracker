3652d5c5442e27ffca73a00f164a10f6f9e80cf3
Felix Thea <felix.thea@placeexchange.com>
2025-03-18 19:27:30 -0400

    override time block

--- app/services/all_review_generator.rb
+++ app/services/all_review_generator.rb
@@ -2,10 +2,11 @@ class AllReviewGenerator
   OPENAI_URL = "https://api.openai.com/v1/chat/completions"
   SEND_HOUR = 18 # 6pm
 
-  def initialize(start_time:, end_time:, review_type:)
+  def initialize(start_time:, end_time:, review_type:, skip_time_restriction: false)
     @start_time = start_time
     @end_time = end_time
     @review_type = review_type
+    @skip_time_restriction = skip_time_restriction
 
     @initial_prompt = case @review_type
     when "WEEKLY"
@@ -23,8 +24,8 @@ class AllReviewGenerator
     User.all.each do |user|
       time_in_user_tz = Time.current.in_time_zone(user.timezone)
 
-      # Only send this report if it's Friday at 6pm the user's timezone
-      if time_in_user_tz.hour == SEND_HOUR && time_in_user_tz.wday == 5
+      # Only send this report if it's Friday at 6pm the user's timezone or if we are skipping the time restriction
+      if @skip_time_restriction || (time_in_user_tz.hour == SEND_HOUR && time_in_user_tz.wday == 5)
         # In the future we may ask the user to specify which expectation to use
         most_recent_expectation = Expectation.where(user_id: user.id).order(created_at: :desc).first
         review_generator = ReviewGenerator.new(
@@ -39,7 +40,7 @@ class AllReviewGenerator
         review = review_generator.call
 
         if !review.nil?
-          MailgunService.send_email("felixthea@gmail.com", "Hello!", review.result)
+          MailgunService.send_email("felix@easyperf.co", "Your EasyPerf report!", review.result)
         end
       end
     end
